3b7adb73fa7e4410c0a2c330ad516017fd92d348
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-12-08 11:56:06 +0530

    feat: Demonstrated usage of React elements within functional components

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const heading = (
+  <h1 id="title" keys="h1">
+    Hi Krish!!
+  </h1>
+);
+
 const HeaderComponent = () => {
   return (
     <div>
+      {heading}
       <h1>Namaste React Functional Component</h1>
       <h2>This is h2 tag</h2>
     </div>
