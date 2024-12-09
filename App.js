d8f0e21bb0468720e7026f10824a114ef61d6b7f
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-12-09 18:18:03 +0530

    feat: Initialized navbar component with basic structure for site

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 const Title = () => (
-  <h1 id="title" keys="h1">
-    Hi Krish!!
-  </h1>
+  <a href="/">
+    <img
+      className="logo"
+      alt="logo"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGgC-F1sV7Bis8bwmZSQj7VEhTM8gV0FxBHg&s"
+    />
+  </a>
 );
 
-const HeaderComponent = () => {
+const Header = () => {
   return (
-    <div>
-      <Title /> {/* This is also known as component composition */}
-      <h1>Namaste React Functional Component</h1>
-      <h2>This is h2 tag</h2>
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
+const Body = () => {
+  return <h4>Body</h4>;
+};
+const Footer = () => {
+  return <h4>Footer</h4>;
+};
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeaderComponent />);
+root.render(<AppLayout />);
