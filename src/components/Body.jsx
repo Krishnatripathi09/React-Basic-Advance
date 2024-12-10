60f76ae6e7530549bf91800cda7bb117dfbbb7a7
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-12-10 16:15:07 +0530

    feat: implemented basic search functionality using useState to manage user input state in the search bar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,20 @@
 import { RestrauntCard } from "./RestrauntCard";
+import { useState } from "react";
+
+
 
 const Body = () => {
+    const [searchInput,setSearchInput]=useState("KFC")
+    
     return (<>
     <div className="search-container">
- <input type="text" className="search-input" placeholder="search" value=""/>
+ <input type="text" className="search-input" placeholder="search" value={searchInput}
+ onChange={(e)=>{
+    setSearchInput(e.target.value)
+   
+ }}
+ 
+ />
  <button className="search-button">Search</button>
     </div>
 
