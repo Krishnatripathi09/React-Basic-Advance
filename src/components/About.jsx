ec7293a45543b8843725fedb332d52b194058555
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-12-18 16:35:39 +0530

    feat: studied the componentDidMount lifecycle method in class component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,13 @@ import React from "react"
 import UserClass from "./UserClass";
 import User from "./User";
 const About = () => {
+
+
   return (
     <div>
       <h1>About</h1>
       <h2>This is React Learning</h2>
+      <h3>{console.log("Inside Parent component")}</h3>
       <User />
       <UserClass name={"Krishna Tripathi"} Location={"Maharashtra"}/>
     </div>
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -2,15 +2,24 @@ import React from "react"
 class UserClass extends React.Component {
 constructor(props){
     super(props);
-
+console.log("Constructor is called ")
    this.state={
    count:22
    }
 }
 
+componentDidMount(){
+console.log("Component Did Mount is called ")
+}
+
+//We make api calls inside component Did Mount because it is implemented last in class class based components
+//like first the constructor of class will be called then the render of the class will be called and if that class 
+//has child class then execution will go to child class and then constructor of child class be called
+// then render of child class will be called and then componentDidMount of child class will be called 
+//and then the componentDidMount of parent class will be called 
 
     render(){
-
+console.log("render is called")
         const {name,Location} =this.props
         const {count}=this.state;
         return (
