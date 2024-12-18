fdce45aea0b595fd9367f75b29e475a0c57ba15f
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-12-18 14:01:35 +0530

    feat: accessed and destructured props in class-based component using constructor, super, and this.props

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,13 @@
 import React from "react"
+import UserClass from "./UserClass";
 import User from "./User";
-
 const About = () => {
   return (
     <div>
       <h1>About</h1>
       <h2>This is React Learning</h2>
       <User />
+      <UserClass name={"Krishna Tripathi"} Location={"Maharashtra"}/>
     </div>
   )
 };
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,13 +1,22 @@
-class UserClass extends React.Component{
+import React from "react"
+class UserClass extends React.Component {
+constructor(props){
+    super(props);
+
+    console.log(props)
+}
+
 
     render(){
+
+        const {name,Location} =this.props
         return (
             <div className="user-card">
-              <h2>Name:Krishnakant</h2>
-              <h2>Location:Pune</h2>
+              <h2>Name:{name}</h2>
+              <h2>Location:{Location}</h2>
               <h2>Contact: 9284640248</h2>
             </div>
           )
     }
-    
 }
+export default UserClass;
