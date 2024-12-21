39188c59c9f40b20c06b019d954e6a480f1d0088
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-12-21 17:59:42 +0530

    feat: Added 'Arrow' button to restaurant card UI for category-based filtering for food items, and added 'Add' button functionality to food items for easier selection and ordering.

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,9 +1,36 @@
 import React from "react"
+import { CDN_URL } from "../utils/constants";
 
-const ItemList = () => {
+const ItemList = ({items}) => {
+
   return (
     <div>
-      Category Items
+   
+    {items.map(item => <div key={item.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 text-left flex">
+   
+        <div className="w-8/12">
+        <div className="py-2">
+           <span>{item.card.info.name}</span>
+        <span> - ₹{item.card.info.price ? item.card.info.price/100 : item.card.info.defaultPrice/100}</span>
+        </div>
+       <p className="text-xs">{item.card.info.description}</p>
+
+       
+       </div>
+
+       <div className="w-4/12 p-4 relative">
+  <img src={CDN_URL + item.card.info.imageId} className="w-full rounded-lg" />
+  <button className="absolute inset-x-6 bottom-2 mx-auto px-3 py-0.5 bg-white text-green-600 text-sm font-bold rounded-lg shadow-md border border-gray-300">
+    ADD +
+  </button>
+</div>
+
+
+
+       
+
+    </div>)}
+  
     </div>
   )
 };
--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -1,18 +1,26 @@
-import React from "react"
+import {useState} from "react"
 import ItemList from "./ItemList";
 
 const RestaurantCategory = ({data}) => {
-    console.log(data)
+    
+    const [showItems,setShowItem] =useState(false)
+
+    const handleClick =()=>{
+        setShowItem(true)
+    }
   return (
     
     <div>
                {/* Header */}
-               <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 flex justify-between ">
+               <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
+                <div className=" flex justify-between cursor-pointer" onClick={handleClick}>
                 <span className="font-bold text-lg">{data.title}({data.itemCards.length})</span>
-                <span>🔽</span>
+                <span o>🔽</span>
+                </div>
+                {showItems && <ItemList items={data.itemCards}/>}
                </div>
 
-               <ItemList items={data.itemCards}/>
+              
     </div>
   )
 };
