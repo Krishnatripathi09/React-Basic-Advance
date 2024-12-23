3e18e946aea9aa5b9c03c23a629625241a577ff8
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-12-23 17:35:22 +0530

    test: Add test case to verify all input boxes on Contact Us page
    - Wrote a test case to render the Contact Us page and retrieve all input boxes using screen.getAllByRole(textbox).
    - Ensured the test validates the presence of all input fields on the page.

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,4 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import {
+  getAllByRole,
+  getByRole,
+  render,
+  screen,
+} from "@testing-library/react";
 import ContactUs from "../ContactUs";
 import "@testing-library/jest-dom";
 test("Should load contact us page", () => {
@@ -7,4 +12,18 @@ test("Should load contact us page", () => {
   const heading = screen.getByRole("heading");
 
   expect(heading).toBeInTheDocument();
+});
+test("Should load Button Inside Contact us page", () => {
+  render(<ContactUs />);
+
+  const Button = screen.getByText("Submit");
+
+  expect(Button).toBeInTheDocument();
+});
+
+test("should load 2 input boxes on Contact Page", () => {
+  render(<ContactUs />);
+  const inputBoxes = screen.getAllByRole("textbox");
+
+  //expect(inputBoxes)
 });
