852f9d386741a9791c26061ca167b4f6a0873d67
Krishnakant Tripathi <58720279+Krishnatripathi09@users.noreply.github.com>
2025-07-11 16:41:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -85,3 +85,6 @@ const appRouter = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRouter} />);
+
+export default appRouter;
+
